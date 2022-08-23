35ea77b9979f537e2a465c8a1305b45b1b185828
Phuong Tran <phuong.tran@zalora.com>
2022-08-23 18:18:45 +0700

    update

--- dblib/src/main/java/com/db/lib/dao/DaoDeleteTemplate.kt
+++ dblib/src/main/java/com/db/lib/dao/DaoDeleteTemplate.kt
@@ -3,17 +3,50 @@ package com.db.lib.dao
 import androidx.room.Delete
 import androidx.room.Transaction
 import com.db.lib.dml.EntityDeleteTemplate
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Maybe
+import io.reactivex.rxjava3.core.Single
 
 interface DaoDeleteTemplate<E> : EntityDeleteTemplate<E> {
     @Transaction
     @Delete
-    override suspend fun delete(entity: E)
+    override suspend fun delete(entity: E) : Int
 
     @Transaction
     @Delete
-    override suspend fun delete(vararg entities: E)
+    override suspend fun delete(vararg entities: E) : Int
 
     @Transaction
     @Delete
-    override suspend fun delete(entities: List<E>)
+    override suspend fun delete(entities: List<E>) : Int
+
+    // RX
+    @Transaction
+    @Delete
+    override fun deleteCompletable(entity: E): Completable
+    @Transaction
+    @Delete
+    override fun deleteCompletable(vararg entities: E): Completable
+    @Transaction
+    @Delete
+    override fun deleteCompletable(entities: List<E>): Completable
+    @Transaction
+    @Delete
+    override fun deleteMaybe(entity: E): Maybe<Int>
+    @Transaction
+    @Delete
+    override fun deleteMaybe(vararg entities: E): Maybe<Int>
+    @Transaction
+    @Delete
+    override fun deleteMaybe(entities: List<E>): Maybe<Int>
+
+    @Transaction
+    @Delete
+    override fun deleteSingle(entity: E): Single<Int>
+    @Transaction
+    @Delete
+    override fun deleteSingle(vararg entities: E): Single<Int>
+    @Transaction
+    @Delete
+    override fun deleteSingle(entities: List<E>): Single<Int>
 }
--- dblib/src/main/java/com/db/lib/dml/EntityDeleteTemplate.kt
+++ dblib/src/main/java/com/db/lib/dml/EntityDeleteTemplate.kt
@@ -1,7 +1,25 @@
 package com.db.lib.dml
 
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Maybe
+import io.reactivex.rxjava3.core.Single
+
 interface EntityDeleteTemplate<E> {
-    suspend fun delete(entity: E)
-    suspend fun delete(vararg entities: E)
-    suspend fun delete(entities: List<E>)
+    suspend fun delete(entity: E) : Int
+    suspend fun delete(vararg entities: E) : Int
+    suspend fun delete(entities: List<E>) : Int
+
+    fun deleteCompletable(entity: E) : Completable
+    fun deleteCompletable(vararg entities: E) : Completable
+    fun deleteCompletable(entities: List<E>) : Completable
+
+    fun deleteMaybe(entity: E) : Maybe<Int>
+    fun deleteMaybe(vararg entities: E) : Maybe<Int>
+    fun deleteMaybe(entities: List<E>) : Maybe<Int>
+
+
+    fun deleteSingle(entity: E) : Single<Int>
+    fun deleteSingle(vararg entities: E) : Single<Int>
+    fun deleteSingle(entities: List<E>) : Single<Int>
+
 }
