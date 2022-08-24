bfee33a489e5aba9de4a7af65e316668167b8354
Phuong Tran <phuong.tran@zalora.com>
2022-08-24 08:50:07 +0700

    update

--- app/src/main/java/com/pt/room/lib/db/StudentEntity.kt
+++ app/src/main/java/com/pt/room/lib/db/StudentEntity.kt
@@ -3,11 +3,11 @@ package com.pt.room.lib.db
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-
+import com.db.lib.entity.BaseEntity
 @Entity
 data class StudentEntity(
     @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id")
-    val id: Long = 0,
+    override val id: Long = 0,
     val name: String,
     val age: Int
-)
+): BaseEntity<Long>
---
+++ dblib/src/main/java/com/db/lib/entity/BaseEntity.kt
@@ -0,0 +1,5 @@
+package com.db.lib.entity
+
+interface BaseEntity<ID> {
+    val id: ID
+}
